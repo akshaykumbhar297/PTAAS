fa2c7eeb2377d24a2d45ec51284f590f3c59837f
kishangupta94 <akshay.kumbhar297@gmail.com>
2022-09-22 17:13:17 +0530

    PTAAS

--- PTAAS.sh
+++ PTAAS.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash launch_burp.sh http testphp.vulweb.com 80 / mohinikumbhar44@gmail.com --user-config-file=user.json
+bash launch_burp.sh http testphp.vulweb.com 80 / akshay.kumbhar297@gmail.com --user-config-file=user.json
